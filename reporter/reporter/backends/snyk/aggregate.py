31f52ba090023a0dd985f2aee39ec4cb4935596d
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-03-14 13:53:20 +0100

    Add vulnerabilities and scan IDs methods

--- reporter/reporter/backends/snyk/aggregate.py
+++ reporter/reporter/backends/snyk/aggregate.py
@@ -1,12 +1,14 @@
 from collections import Counter
 from dataclasses import dataclass
-from loguru import logger
-import numpy as np
-
 from functools import cached_property
-from .model import SnykContainerScan
+from typing import Iterator
 
-from numpy.typing import NDArray
+import numpy as np
+from loguru import logger
+
+from .model import SnykContainerScan, SnykVulnerability
+from ..._types import MplRGBAColor
+
 
 # Use dataclass here since we don't need validation
 @dataclass
@@ -48,30 +50,27 @@ class AggregateScan:
             )
         return scores
 
+    def get_scan_ids(self) -> list[str]:
+        return [scan.id for scan in self.scans]
+
+    def vulnerabilities(self) -> Iterator[SnykVulnerability]:
+        """Generator that yields vulnerabilities from all scans."""
+        for scan in self.scans:
+            for vuln in scan.vulnerabilities:
+                yield vuln
+
     # TODO: Exception handling for median, mean and std
-
-    @cached_property
-    def cvss_median(self) -> float:
-        return float(np.median(self.cvss_scores))
-
-    @cached_property
-    def cvss_mean(self) -> float:
-        return float(np.mean(self.cvss_scores))
-
-    @cached_property
-    def cvss_sd(self) -> float:
-        return float(np.std(self.cvss_scores))
 
     def most_common_cve(self, n: int) -> list[tuple[str, int]]:
         c: Counter[str] = Counter()
         for scan in self.scans:
-            # we need to fetch all CVEs so we don't pass the n arg here
+            # we need to retrieve all CVEs, so we don't pass the n arg here
             mc = scan.most_common_cve()
             c.update(mc)
         return c.most_common(n)  # only here do we use n
 
-    def get_vuln_age_score_color(self) -> list[tuple[int, float, NDArray]]:
-        l: list[tuple[int, float, NDArray]] = []
+    def get_vuln_age_score_color(self) -> list[tuple[int, float, MplRGBAColor]]:
+        l: list[tuple[int, float, MplRGBAColor]] = []
         for scan in self.scans:
             l.extend(scan.vulnerabilities.get_vulns_age_score_color())
         return l
