10c073e03536d5245a8d838c3a84cbaec92391a9
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-03-17 12:53:48 +0100

    Add /report endpoint

--- reporter/reporter/main.py
+++ reporter/reporter/main.py
@@ -1,27 +1,62 @@
-from fastapi import FastAPI, File, Request
-from fastapi.responses import RedirectResponse
-from loguru import logger
-from pydantic import BaseModel
-import os
-from .backends.snyk import SnykContainerScan
+# NOTE: ONLY SUPPORTS GCP RIGHT NOW
 
-# from .gcp.firestore import get_firestore_client
-from auspex_core.gcp.firestore import get_firestore_client
-from auspex_core.gcp.env import LOGS_COLLECTION_NAME
+
+import io
+from pathlib import Path
+
+
+from fastapi import FastAPI
+from fastapi.exceptions import HTTPException
+from fastapi.responses import StreamingResponse
+from loguru import logger
+from auspex_core.gcp.storage import fetch_json_blob
+from auspex_core.gcp.env import LOGS_COLLECTION_NAME, SCANS_BUCKET_NAME
+from auspex_core.gcp.firestore import get_document
+
+from .backends.snyk.model import SnykContainerScan
+from .frontends.latex import create_document
+from .models import ReportSingleIn
 
 app = FastAPI()
 
-# ONLY SUPPORTS GCP RIGHT NOW
+# Add mock routes for internal development
+from contextlib import suppress
+
+with suppress(ImportError):
+    from ._mock import mockrouter
+
+    app.include_router(mockrouter)
 
 
-@app.post("/pdf/{document_id}")
-async def generate_report(document_id: str):
-    db = get_firestore_client()
-    docpath = f"{LOGS_COLLECTION_NAME}/{document_id}"
-    logger.debug(f"Fetching {docpath}")
-    d = db.document(docpath)
-    doc = await d.get()
-    return doc.to_dict()
+@app.post("/report")
+async def generate_report(r: ReportSingleIn):
+    # Retrieve document
+    try:
+        doc = await get_document(LOGS_COLLECTION_NAME, r.document_id)
+    except ValueError as e:
+        raise HTTPException(
+            404,
+            detail=e.args[0] if e.args else f"Scan with id {r.document_id} not found",
+        )
+
+    # Get scan file from bucket
+    blobname = doc.get("blob")
+    if not blobname:
+        raise HTTPException(
+            422, f"Document {r.document_id} has no scan file associated with it."
+        )
+    obj = await fetch_json_blob(SCANS_BUCKET_NAME, blobname)
+
+    # Parse scan log and create report
+    scan = SnykContainerScan(**obj.content)
+    latex_document = await create_document(scan)
+    document_path = Path(f"{latex_document.default_filepath}.pdf")
+    if not document_path.exists():
+        raise HTTPException(500, "Failed to generate report.")
+
+    # Send report file back as a streaming response
+    with open(document_path, "rb") as f:
+        return StreamingResponse(io.BytesIO(f.read()), media_type="application/pdf")
 
 
 @app.post("/pdf/aggregate")
