836b0041683a66c4361cf914f430c75c2dc9a6af
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-03-17 12:58:39 +0100

    Add environment variables for tests (WIP)

--- reporter/tests/conftest.py
+++ reporter/tests/conftest.py
@@ -1,3 +1,13 @@
+import os
+
+# TODO fix this
+os.environ.setdefault(
+    "SERVICE_ACCOUNT_KEYFILE",
+    "/Volumes/GoogleDrive/My Drive/Skole/2022V/Bachelor/repo/.keys/reporter_local.json",
+)
+os.environ.setdefault("LOGS_COLLECTION_NAME", "auspex-logs")
+os.environ.setdefault("SCANS_BUCKET_NAME", "auspex-scans")
+
 from typing import Any
 
 from hypothesis import strategies as st
