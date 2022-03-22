da8145981ec557c2b0802a2ab4c0213e0bde9644
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-03-22 16:18:03 +0100

    Add /aggregate endpoint

--- reporter/reporter/main.py
+++ reporter/reporter/main.py
@@ -4,18 +4,19 @@
 import io
 from pathlib import Path
 
-
+from auspex_core.gcp.env import LOGS_COLLECTION_NAME, SCANS_BUCKET_NAME
+from auspex_core.gcp.firestore import get_document
 from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
 from fastapi.responses import StreamingResponse
 from loguru import logger
-from auspex_core.gcp.storage import fetch_json_blob
-from auspex_core.gcp.env import LOGS_COLLECTION_NAME, SCANS_BUCKET_NAME
-from auspex_core.gcp.firestore import get_document
 
+from .db import log_scan
 from .backends.snyk.model import SnykContainerScan
 from .frontends.latex import create_document
-from .models import ReportSingleIn
+from .models import ReportAggregateIn, ReportSingleIn
+from .utils.firestore import get_firestore_document
+from .utils.storage import get_object_from_document
 
 app = FastAPI()
 
@@ -28,27 +29,21 @@ with suppress(ImportError):
     app.include_router(mockrouter)
 
 
+async def scan_from_docid(docid: str, collection: str) -> SnykContainerScan:
+    # DB Document
+    doc = await get_firestore_document(docid, collection)
+    # Download blob from document
+    obj = await get_object_from_document(doc)
+    # Parse scan log
+    assert hasattr(obj.blob, "id")
+    return SnykContainerScan(**obj.content, id=obj.blob.id)
+
+
 @app.post("/report")
 async def generate_report(r: ReportSingleIn):
-    # Retrieve document
-    try:
-        doc = await get_document(LOGS_COLLECTION_NAME, r.document_id)
-    except ValueError as e:
-        raise HTTPException(
-            404,
-            detail=e.args[0] if e.args else f"Scan with id {r.document_id} not found",
-        )
+    scan = await scan_from_docid(r.document_id, r.collection)
+    await log_scan(scan)
 
-    # Get scan file from bucket
-    blobname = doc.get("blob")
-    if not blobname:
-        raise HTTPException(
-            422, f"Document {r.document_id} has no scan file associated with it."
-        )
-    obj = await fetch_json_blob(SCANS_BUCKET_NAME, blobname)
-
-    # Parse scan log and create report
-    scan = SnykContainerScan(**obj.content)
     latex_document = await create_document(scan)
     document_path = Path(f"{latex_document.default_filepath}.pdf")
     if not document_path.exists():
@@ -59,7 +54,24 @@ async def generate_report(r: ReportSingleIn):
         return StreamingResponse(io.BytesIO(f.read()), media_type="application/pdf")
 
 
-@app.post("/pdf/aggregate")
-async def generate_aggregate_report(snyk_scan: SnykContainerScan):
-    print(snyk_scan.severity_v3())
-    return snyk_scan.architecture
+@app.post("/aggregate")
+async def generate_aggregate_report(r: ReportAggregateIn):
+    failed = []  # type: list[str]
+    scans = []  # type: list[SnykContainerScan]
+    for docid in r.document_ids:
+        try:
+            scan = await scan_from_docid(docid, r.collection)
+        except Exception:
+            logger.exception(f"Failed to retrieve document with ID {docid}")
+            failed.append(docid)
+        else:
+            scans.append(scan)
+
+    if failed:
+        msg = f"Failed to retrieve the following documents: {failed}"
+        logger.error(msg)
+        if not r.ignore_failed or not scans:
+            # TODO: clarify to user if _all_ requested scans fail with ignore_failed=True
+            raise HTTPException(500, msg)
+
+    # Make aggregate report
