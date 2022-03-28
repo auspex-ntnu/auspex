6a2b35474295d4191b305d25ac0eace48010c8c1
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-03-28 17:06:50 +0200

    Reporter: Add new types module

---
+++ reporter/reporter/types/__init__.py
@@ -0,0 +1,11 @@
+from .protocols import ScanType, VulnerabilityType, ScanTypeAggregate, ScanTypeSingle
+from .nptypes import NumberType, MplRGBAColor
+
+__all__ = [
+    "ScanType",
+    "VulnerabilityType",
+    "ScanTypeAggregate",
+    "ScanTypeSingle",
+    "NumberType",
+    "MplRGBAColor",
+]
