641740e968271b3ea54a78224cd35d3cd63513d0
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-03-28 17:08:55 +0200

    Add imports to utils module

--- reporter/reporter/utils/__init__.py
+++ reporter/reporter/utils/__init__.py
@@ -1,1 +1,4 @@
 from .matplotlib import get_cvss_color
+from .firestore import get_firestore_document, get_firestore_documents
+from .npmath import mean, median, stdev
+from .storage import get_object_from_document
