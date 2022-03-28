7d57630057d49a1156eda2b42ea242b20380ab22
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-03-28 16:45:32 +0200

    Use google sdk credential autodetection

--- core/auspex_core/gcp/firestore.py
+++ core/auspex_core/gcp/firestore.py
@@ -4,27 +4,9 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from google.cloud.firestore_v1 import DocumentSnapshot
 from google.cloud.firestore_v1.async_client import AsyncClient
-from google.oauth2.service_account import Credentials
 from loguru import logger
 
-from .auth import credentials_from_env
-from .env import GCP_PROJECT, SERVICE_ACCOUNT_KEYFILE
-
-
-def get_credentials() -> Credentials:
-    """Retrieves GCP credentials to be used for firebase authentication.
-
-    Uses application default credentials if running on GCP,
-    otherwise falls back on JSON key file authentication.
-
-    Returns
-    -------
-    `Credentials`
-        Google oauth2 credentials
-    """
-    if SERVICE_ACCOUNT_KEYFILE:
-        return credentials_from_env()
-    return credentials.ApplicationDefault()
+from .env import GCP_PROJECT
 
 
 @cache
@@ -45,7 +27,6 @@ def get_firestore_client() -> firestore.firestore.AsyncClient:
 
     return AsyncClient(
         project=GCP_PROJECT,
-        credentials=get_credentials(),
     )
 
 
@@ -58,14 +39,10 @@ app = firebase_admin.initialize_app(
 )
 
 
-def get_firestore_logs_docpath(collection_name: str, document_id: str) -> str:
-    return f"{collection_name}/{document_id}"
-
-
 async def get_document(collection_name: str, document_id: str) -> DocumentSnapshot:
     db = get_firestore_client()
     # Get firestore document
-    docpath = get_firestore_logs_docpath(collection_name, document_id)
+    docpath = f"{collection_name}/{document_id}"
     logger.debug(f"Fetching {docpath}")
     d = db.document(docpath)
     doc = await d.get()  # type: DocumentSnapshot
