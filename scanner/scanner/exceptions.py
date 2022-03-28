d335a807358ab387b86da57091f72b1d61482ec7
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-03-28 17:05:50 +0200

    Add UserAPIError

--- scanner/scanner/exceptions.py
+++ scanner/scanner/exceptions.py
@@ -3,8 +3,12 @@ from fastapi.exceptions import HTTPException
 
 class HTTPNotFoundException(HTTPException):
     def __init__(self, resource: str, *args, **kwargs):
-        super().__init__(404, f"{resource} not found.", *args, **kwargs)
+        super().__init__(404, f"{resource} not found.", *args, **kwargs)
 
 
 class APIError(Exception):
-    pass
+    """Exception raised in response to serverside errors."""
+
+
+class UserAPIError(Exception):
+    """Exception raised in response to bad input from user."""
