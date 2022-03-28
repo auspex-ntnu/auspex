c168067e9a390d67b1158ffca6a13681e941a329
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-03-28 16:50:50 +0200

    Update reporter main

--- reporter/reporter/main.py
+++ reporter/reporter/main.py
@@ -1,22 +1,39 @@
 # NOTE: ONLY SUPPORTS GCP RIGHT NOW
 
 
+from datetime import timedelta
 import io
+import os
 from pathlib import Path
 
-from auspex_core.gcp.env import LOGS_COLLECTION_NAME, SCANS_BUCKET_NAME
+from auspex_core.gcp.env import (
+    LOGS_COLLECTION_NAME,
+    SCANS_BUCKET_NAME,
+    PARSED_COLLECTION_NAME,
+)
 from auspex_core.gcp.firestore import get_document
 from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
 from fastapi.responses import StreamingResponse
 from loguru import logger
 
+
 from .db import log_scan
 from .backends.snyk.model import SnykContainerScan
 from .frontends.latex import create_document
-from .models import ReportAggregateIn, ReportSingleIn
-from .utils.firestore import get_firestore_document
-from .utils.storage import get_object_from_document
+from .models import ReportRequestIn
+from .types.protocols import ScanTypeSingle
+from .utils.firestore import get_firestore_document, get_prev_scans
+from .utils.storage import get_object_from_document, upload_report_to_bucket
+
+if os.getenv("DEBUG") == "1":
+    import debugpy
+
+    DEBUG_PORT = 5678
+    debugpy.listen(("0.0.0.0", DEBUG_PORT))
+    logger.debug(f"Debugger is listening on port {DEBUG_PORT}")
+    # debugpy.wait_for_client()
+    # debugpy.breakpoint()
 
 app = FastAPI()
 
@@ -29,36 +46,47 @@ with suppress(ImportError):
     app.include_router(mockrouter)
 
 
-async def scan_from_docid(docid: str, collection: str) -> SnykContainerScan:
+async def scan_from_docid(docid: str, collection: str) -> ScanTypeSingle:
     # DB Document
     doc = await get_firestore_document(docid, collection)
     # Download blob from document
     obj = await get_object_from_document(doc)
     # Parse scan log
     assert hasattr(obj.blob, "id")
+
+    # TODO: use image from document
+    # image = doc.get("image")
+    # use timestamp from document
+    # use backend from document
+
+    # TODO: fix typing!! Why does SnykContainerScan not pass as a ScanTypeSingle type?
     return SnykContainerScan(**obj.content, id=obj.blob.id)
 
 
 @app.post("/report")
-async def generate_report(r: ReportSingleIn):
-    scan = await scan_from_docid(r.document_id, r.collection)
+async def generate_report(r: ReportRequestIn):
+    scan = await scan_from_docid(r.document_id[0], r.collection)
     await log_scan(scan)
 
-    latex_document = await create_document(scan)
-    document_path = Path(f"{latex_document.default_filepath}.pdf")
-    if not document_path.exists():
-        raise HTTPException(500, "Failed to generate report.")
+    prev_scans = await get_prev_scans(
+        scan, PARSED_COLLECTION_NAME, max_age=timedelta(weeks=24), ignore_self=True
+    )
 
-    # Send report file back as a streaming response
-    with open(document_path, "rb") as f:
-        return StreamingResponse(io.BytesIO(f.read()), media_type="application/pdf")
+    doc = await create_document(scan, prev_scans)
+    if not doc.path.exists():
+        raise HTTPException(500, "Failed to generate report.")
+    status = await upload_report_to_bucket(
+        doc.path, "auspex-reports"
+    )  # FIXME: use env var
+
+    return {"request": r.dict(), "status": status}
 
 
 @app.post("/aggregate")
-async def generate_aggregate_report(r: ReportAggregateIn):
+async def generate_aggregate_report(r: ReportRequestIn):
     failed = []  # type: list[str]
-    scans = []  # type: list[SnykContainerScan]
-    for docid in r.document_ids:
+    scans = []  # type: list[ScanTypeSingle]
+    for docid in r.document_id:
         try:
             scan = await scan_from_docid(docid, r.collection)
         except Exception:
@@ -73,5 +101,7 @@ async def generate_aggregate_report(r: ReportAggregateIn):
         if not r.ignore_failed or not scans:
             # TODO: clarify to user if _all_ requested scans fail with ignore_failed=True
             raise HTTPException(500, msg)
+
+    return r
 
     # Make aggregate report
