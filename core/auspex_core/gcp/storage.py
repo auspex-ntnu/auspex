dab0a00bfb801ca2e98616fb0725e2de1dee24f5
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-03-28 16:47:28 +0200

    Add function for uploading a file to a bucket

--- core/auspex_core/gcp/storage.py
+++ core/auspex_core/gcp/storage.py
@@ -70,7 +70,12 @@ def get_storage_client() -> StorageWithBackoff:
 
 
 async def fetch_json_blob(bucket_name: str, blob_name: str) -> StorageObject:
+    """Fetches a JSON blob from the given bucket.
+
+    Returns a StorageObject with the blob and the parsed JSON content.
+    """
     async with get_storage_client() as client:
+        # TODO: handle missing bucket
         bucket = client.get_bucket(bucket_name)
         blob = await bucket.get_blob(blob_name)
         content = await blob.download()
@@ -92,6 +97,42 @@ async def fetch_json_blob(bucket_name: str, blob_name: str) -> StorageObject:
         raise
     return StorageObject(blob=blob, content=json_content)
 
+
+# TODO: add backoff?
+async def upload_file_to_bucket(path: Path, bucket_name: str) -> "ObjectStatus":
+    """Uploads a file to the given bucket.
+
+    Returns the metadata of the uploaded blob.
+    """
+    # TODO: don't nest try/excepts
+    try:
+        async with get_storage_client() as client:
+
+            try:
+                # Check if bucket exists
+                bucket = client.get_bucket(bucket_name)
+                await bucket.get_metadata()
+            except aiohttp.ClientResponseError as e:
+                if e.code != 404:
+                    raise
+                # Create bucket if it doesn't exist
+                loop = asyncio.get_event_loop()
+                await loop.run_in_executor(None, create_bucket, bucket_name)
+
+            status = await client.upload_from_filename(
+                bucket_name, path.name, str(path)
+            )
+            logger.debug(status)
+            logger.debug(f"Uploaded file {path} to bucket {bucket_name}")
+    # catch errors
+    except Exception as e:
+        logger.exception(f"Unable to upload file {path} to bucket {bucket_name}", e)
+        raise
+
+    # TODO: investigate if it's safe to use .construct() here
+    return ObjectStatus.construct(**status)
+
+
 def create_bucket(bucket_name: str) -> Bucket:
     """Blocking function that creates a bucket with the given name.
 
@@ -105,3 +146,24 @@ def create_bucket(bucket_name: str) -> Bucket:
     bucket = client.create_bucket(bucket_name)
     logger.info(f"Created bucket {bucket_name}")
     return bucket
+
+
+class ObjectStatus(BaseModel):
+    # TODO: investigate which fields can be cast to int/float/datetime
+    kind: str
+    id: str
+    selfLink: str
+    mediaLink: str
+    name: str
+    bucket: str
+    generation: str
+    metageneration: str
+    contentType: str
+    storageClass: str
+    size: str
+    md5Hash: str
+    crc32c: str
+    etag: str
+    timeCreated: str
+    updated: str
+    timeStorageClassUpdated: str
