477872e5574741a89a7b592e0af339e96f99a0a3
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-03-28 16:44:50 +0200

    Remove credentials_from_env()

--- core/auspex_core/gcp/auth.py
+++ core/auspex_core/gcp/auth.py
@@ -1,18 +1,8 @@
 from pathlib import Path
 from typing import Union
 from google.oauth2.service_account import Credentials
-from .env import SERVICE_ACCOUNT_KEYFILE
+from .env import GOOGLE_APPLICATION_CREDENTIALS
 
 
-def credentials_from_env() -> Credentials:
-    """Instantiates GCP credentials from a JSON key file."""
-    if not SERVICE_ACCOUNT_KEYFILE:
-        return ValueError(
-            "environment variable 'SERVICE_ACCOUNT_KEYFILE' is not set. "
-            "Unable to create credentials from keyfile."
-        )
-    return _credentials_from_keyfile(SERVICE_ACCOUNT_KEYFILE)
-
-
-def _credentials_from_keyfile(filepath: Union[str, Path]) -> Credentials:
+def credentials_from_keyfile(filepath: Union[str, Path]) -> Credentials:
     return Credentials.from_service_account_file(filepath)
