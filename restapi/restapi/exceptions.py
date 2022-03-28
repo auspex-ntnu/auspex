a4249b0c3fc55b590a1513ac9f4a42c67b223131
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-03-28 17:21:32 +0200

    RESTAPI: Add exception handler documentation

--- restapi/restapi/exceptions.py
+++ restapi/restapi/exceptions.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI, Request
 from google.api_core.exceptions import FailedPrecondition
 from fastapi.responses import PlainTextResponse
+from loguru import logger
 
 
 def handle_FailedPrecondition(
     request: Request, exc: FailedPrecondition
 ) -> PlainTextResponse:
+    """Handles FailedPrecondition exceptions which usually stem from
+    failed or malformed Firestore queries."""
+    logger.error(exc)  # NOTE: can we just log the exception like this?
     return PlainTextResponse(str(exc.message), status_code=500)
 
 
 def install_handlers(app: FastAPI):
+    """Installs custom exception handlers for the FastAPI app."""
     handlers = {
         FailedPrecondition: handle_FailedPrecondition,
     }
