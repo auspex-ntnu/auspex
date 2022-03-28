0a21f53f949ac702719f05ada37803f0c6981483
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-03-28 17:11:28 +0200

    Reporter: Add integration tests (not working)

--- reporter/tests/test_app.py
+++ reporter/tests/test_app.py
@@ -1,17 +1,32 @@
 from fastapi.testclient import TestClient
+import pytest
 
 from reporter.main import app
 
-client = TestClient(app)
+test_client = TestClient(app)
 
 
 def test_generate_report() -> None:
-    resp = client.post(
+    resp = test_client.post(
         "/report",
         json={
             "format": "latex",
-            "document_id": "6yh6rPCX7hFgAgUIB2Wo",
+            "document_id": "cCvih5GoS5ZTQV2GZN5G",
+            "collection": "auspex-logs-test",
         },
     )
     assert resp.status_code == 200
     assert resp.headers["content-type"] == "application/pdf"
+
+
+def test_generate_report_invalid_id() -> None:
+    resp = test_client.post(
+        "/report",
+        json={
+            "format": "latex",
+            "document_id": "some_invalid_firestore_id",
+            "collection": "auspex-logs-test",
+        },
+    )
+    assert resp.status_code == 404
+    assert "not found" in resp.text
