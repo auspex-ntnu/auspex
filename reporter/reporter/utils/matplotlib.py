5d4b95971bc8c52952c30f42beee2e2e68af0c90
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-03-28 17:12:41 +0200

    Reporter: Fix nptypes import path

--- reporter/reporter/utils/matplotlib.py
+++ reporter/reporter/utils/matplotlib.py
@@ -1,11 +1,12 @@
 from matplotlib.cm import get_cmap
 from matplotlib.colors import Colormap
 
-from .._types import MplRGBAColor
+from ..types.nptypes import MplRGBAColor
 
+# We use the RdYlGn CMAP but reverse it so it goes from Green to Red
 DEFAULT_CMAP = get_cmap("RdYlGn")
 DEFAULT_CMAP = DEFAULT_CMAP.reversed()
-DEFAULT_CMAP._init()
+DEFAULT_CMAP._init()  # call _init() so we can access the _lut attribute
 
 
 def get_cvss_color(score: float, cmap: Colormap = DEFAULT_CMAP) -> MplRGBAColor:
