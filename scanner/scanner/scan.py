0526cf3d1731d3e1f3e2256ca57280c80986cd4d
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-03-30 19:28:42 +0200

    Raise UserAPIError instead of APIError

--- scanner/scanner/scan.py
+++ scanner/scanner/scan.py
@@ -1,23 +1,23 @@
 import asyncio
+
 from loguru import logger
 
 from .backends.snyk import run_snyk_scan
-from .exceptions import APIError
+from .exceptions import UserAPIError
 from .types import ScanResultsType
 
-backends = {"snyk": run_snyk_scan}
+BACKENDS = {"snyk": run_snyk_scan}
 
 
 async def scan_container(image: str, backend: str) -> ScanResultsType:
     """Scans a container image using the selected scanning backend."""
     loop = asyncio.get_event_loop()
-    return await loop.run_in_executor(None, _scan_container, image, backend)
+    return await loop.run_in_executor(None, _do_scan_container, image, backend)
 
 
-def _scan_container(image: str, backend: str) -> ScanResultsType:
-    scan_func = backends.get(backend)
+def _do_scan_container(image: str, backend: str) -> ScanResultsType:
+    scan_func = BACKENDS.get(backend)
     if not scan_func:
-        raise APIError(f"Unknown container analysis backend: '{backend}'")
-
+        raise UserAPIError(f"Unknown container analysis backend: '{backend}'")
     logger.debug(f"Starting scan of image '{image}'")
     return scan_func(image)
