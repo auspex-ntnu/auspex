3802c3d8de70f366b89f984128998cfc72373314
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-04-01 17:09:37 +0200

    Scanner: Move GCR functionality to core.

---
+++ core/auspex_core/gcp/gcr.py
@@ -0,0 +1,118 @@
+"""Module defining Google Container Registry API functions."""
+
+# https://stackoverflow.com/questions/61465794/docker-sdk-with-google-container-registry
+
+import asyncio
+import os
+
+import google.auth
+import google.auth.transport.requests
+import httpx
+from google.oauth2 import service_account
+from google.oauth2.service_account import Credentials
+from loguru import logger
+from pydantic import ValidationError
+
+from ..models.gcr import (
+    CatalogResponse,
+    ImageInfo,
+    ImageNameMode,
+    TagsResponse,
+    ImageVersionInfo,
+)
+
+
+def split_image_version(image: str) -> ImageVersionInfo:
+    """Split name and tag/digest from an image name.
+
+    Example:
+        >>> split_image_version("gcr.io/ntnu-student-project/auspex:latest")
+        ImageVersionInfo(
+            image='gcr.io/ntnu-student-project/auspex',
+            tag_or_digest='latest',
+            mode=ImageNameMode.TAG,
+            delimiter=":"
+        )
+    """
+    for c in ["@", ":"]:
+        if c in image:
+            image, tag_or_digest = image.split(c, maxsplit=1)
+            mode = ImageNameMode.DIGEST if c == "@" else ImageNameMode.TAG
+            return ImageVersionInfo(
+                image=image,
+                tag_or_digest=tag_or_digest,
+                mode=mode,
+                delimiter=c,
+            )
+    return ImageVersionInfo(image=image, mode=ImageNameMode.NONE)
+
+
+async def get_image_info(image: str) -> ImageInfo:
+    """Get information about a Container image from Google Container Registry.
+
+    Example:
+        >>> get_image_info("gcr.io/ntnu-student-project/auspex:latest")
+        ImageInfo(
+            image_size_bytes='0',
+            layer_id='',
+            mediaType='application/vnd.docker.distribution.manifest.v2+json',
+            tag=['latest'],
+            created=datetime.datetime(2020, 4, 23, 14, 0, 0, tzinfo=tzutc()),
+            uploaded=datetime.datetime(2020, 4, 23, 14, 0, 0, tzinfo=tzutc()),
+            image_id='sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+            image='gcr.io/ntnu-student-project/auspex'
+        )
+    """
+    # resp = await get_repositories("ntnu-student-project")
+    versioninfo = split_image_version(image)
+    # repo = resp.get_repository(image)
+
+    # TODO: determine repository to search through
+
+    credentials = await credentials_from_file()
+    async with httpx.AsyncClient() as client:
+        r = await client.get(
+            "https://eu.gcr.io/v2/ntnu-student-project/auspex/scanner/tags/list",
+            auth=("_token", credentials.token),
+        )
+
+    try:
+        tagsresp = TagsResponse.parse_obj(r.json())
+    except ValidationError:
+        logger.error(f"Failed to parse response from registry: {r.text}")
+        raise ValueError(f"Image '{image}' not found in registry")  # or?
+    image_info = tagsresp.manifest.get_image_metadata(versioninfo)
+    # inject the image name (without tag) into the image info
+    image_info.image = versioninfo.image
+    return image_info
+
+
+# list repositories
+async def get_repositories(project_id: str = None) -> CatalogResponse:
+    """Get a list of repositories in a project."""
+    credentials = await credentials_from_file()
+    async with httpx.AsyncClient() as client:
+        r = await client.get(
+            "https://eu.gcr.io/v2/_catalog", auth=("_token", credentials.token)
+        )
+        resp = CatalogResponse.parse_obj(r.json())
+        return resp
+    # TODO determine which project to use
+
+
+async def credentials_from_file() -> Credentials:
+    """Get credentials from a service account file and prime it with a token."""
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(None, _do_credentials_from_file)
+
+
+def _do_credentials_from_file() -> Credentials:
+    """Get credentials from a service account file and prime it with a token."""
+    # https://stackoverflow.com/a/67069710
+    credentials = service_account.Credentials.from_service_account_file(
+        os.getenv("GOOGLE_APPLICATION_CREDENTIALS"),
+        scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    )
+    auth_req = google.auth.transport.requests.Request()
+    credentials.refresh(auth_req)
+    return credentials
---
+++ core/auspex_core/models/gcr.py
@@ -0,0 +1,144 @@
+from datetime import datetime
+from enum import Enum, auto
+from typing import Any, NamedTuple, Optional
+
+from pydantic import BaseModel, Field, root_validator, validator
+
+from ..utils.time import timestamp_ms_to_datetime
+
+
+class CatalogResponse(BaseModel):
+    """Represents the response to a docker repository catalog request.
+
+    In our case: https://eu.gcr.io/v2/_catalog
+
+    See: https://docs.docker.com/registry/spec/api/#listing-repositories
+    """
+
+    repositories: list[str]
+
+    def get_repository(self, image: str) -> str:
+        """Get the repository name for an image"""
+        for repo in self.repositories:
+            if repo in image:
+                return repo
+        raise ValueError(f"Image '{image}' not found in registry")
+
+
+class ImageInfo(BaseModel):
+    """Represents information about a single Docker image."""
+
+    image_size_bytes: str = Field(..., alias="imageSizeBytes")
+    layer_id: str = Field(..., alias="layerId")
+    mediaType: str = Field(..., alias="mediaType")
+    tag: list[str]
+    created: datetime = Field(..., alias="timeCreatedMs")
+    uploaded: datetime = Field(..., alias="timeUploadedMs")
+    digest: Optional[str]  # injected by ImageManifest (see its root_validator)
+    image: Optional[str]  # injected by get_image_info()
+    # TODO: add name:tag used to reference this image
+
+    # Parse the timestamps in milliseconds to datetime objects
+    _validate_created = validator("created", pre=True, allow_reuse=True)(
+        timestamp_ms_to_datetime
+    )
+    _validate_uploaded = validator("uploaded", pre=True, allow_reuse=True)(
+        timestamp_ms_to_datetime
+    )
+
+
+class ImageTimeMode(Enum):
+    """Time mode for a Docker image"""
+
+    CREATED = "created"
+    UPLOADED = "uploaded"
+
+
+class ImageNameMode(Enum):
+    """Name mode for a Docker image"""
+
+    DIGEST = auto()
+    TAG = auto()
+    NONE = auto()
+
+
+class ImageVersionInfo(NamedTuple):
+    """Parameters for getting information about a Docker image version."""
+
+    image: str
+    mode: ImageNameMode
+    tag_or_digest: Optional[str] = None
+    delimiter: Optional[str] = None
+
+
+class ImageManifest(BaseModel):
+    """Represents the manifest for a Docker image."""
+
+    __root__: dict[str, ImageInfo]
+
+    @root_validator
+    def inject_digest(
+        cls, values: dict[str, dict[str, ImageInfo]]
+    ) -> dict[str, dict[str, ImageInfo]]:
+        """Inject the sha256 digest into the ImageInfo object"""
+        # Each key is in the dictionary is the image's sha256 digest
+        # We assign this value to the ImageInfo object's digest attribute
+        for k, v in values["__root__"].items():
+            v.digest = k
+        return values
+
+    def get_image_metadata(self, image_info: ImageVersionInfo) -> ImageInfo:
+        """Get the metadata for an image"""
+        # Try to find image by digest
+        # Example: "repo/image@sha256:digest"
+        if image_info.mode == ImageNameMode.DIGEST and image_info.tag_or_digest:
+            img = self.__root__.get(image_info.tag_or_digest)
+            if img is not None:
+                return img
+
+        # Try to find most recent image with the given tag
+        # Example: "repo/image:tag"
+        if image_info.mode == ImageNameMode.TAG:
+            # create sorted list of images from most to least recent
+            images = sorted(
+                self.__root__.values(), key=lambda i: i.created, reverse=True
+            )
+            for img in images:
+                if image_info.tag_or_digest in img.tag:
+                    return img
+
+        # Use latest image if no tag or digest
+        # Example: "repo/image"
+        if image_info.mode == ImageNameMode.NONE:
+            for img in self.__root__.values():
+                # TODO: verify that only one image can have the tag 'latest' at any given time
+                if "latest" in img.tag:
+                    return img
+
+        raise ValueError(f"Image '{image_info.image}' not found in registry")
+
+    def get_newest_image(
+        self, mode: ImageTimeMode = ImageTimeMode.UPLOADED
+    ) -> ImageInfo:
+        # TODO: delete method
+        """Get the newest image in the manifest"""
+        images = list(self.__root__.values())
+        if mode == mode.CREATED:
+            images.sort(key=lambda image: image.created)
+        elif mode == mode.UPLOADED:
+            images.sort(key=lambda image: image.uploaded)
+        return images[-1]
+
+
+class TagsResponse(BaseModel):
+    """Represents the response to a docker repository tags request.
+
+    In our case: "https://eu.gcr.io/v2/<project>/<image>/tags/list"
+
+    See: https://docs.docker.com/registry/spec/api/#listing-image-tags
+    """
+
+    name: str
+    tags: list[str]
+    child: list[Any]
+    manifest: ImageManifest
