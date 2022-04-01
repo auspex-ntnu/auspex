67dd590b4a62b8331c6266d88ec981aab32087f1
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-04-01 17:11:06 +0200

    Core: Add validators module

---
+++ core/auspex_core/models/validators.py
@@ -0,0 +1,10 @@
+"""Module for reusable Pydantic validators."""
+
+from typing import TypeVar
+
+
+IntFloatT = TypeVar("IntFloatT", int, float)
+
+
+def ensure_nonnegative(v: IntFloatT) -> IntFloatT:
+    return abs(v)
