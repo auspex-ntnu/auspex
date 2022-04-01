a2113254952a80054f32d3807c4a149efb87566d
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-04-01 17:12:59 +0200

    Core: remove unused app creds import

--- core/auspex_core/gcp/auth.py
+++ core/auspex_core/gcp/auth.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from typing import Union
 from google.oauth2.service_account import Credentials
-from .env import GOOGLE_APPLICATION_CREDENTIALS
 
 
 def credentials_from_keyfile(filepath: Union[str, Path]) -> Credentials:
