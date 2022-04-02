91e02c7ffe920e747da08b7b1ec1fdc2ed1977c2
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-04-02 15:13:44 +0200

    Logger: add tests

---
+++ functions/logger/gcp/conftest.py
@@ -0,0 +1,6 @@
+import os
+
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./test_credentials.json"
+os.environ["GCP_PROJECT"] = "test_project"
+os.environ["BUCKET_SCANS"] = "test_bucket_scans"
+os.environ["COLLECTION_LOGS"] = "test_collection_logs"
