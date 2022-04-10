625a0b8bfed98edaeaec218bdcc90d320f061a13
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-04-10 21:44:42 +0200

    Scanner: move models to separate module

--- scanner/scanner/main.py
+++ scanner/scanner/main.py
@@ -10,12 +10,14 @@ from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse
 from loguru import logger
 from pydantic import BaseModel, ValidationError
+from auspex_core.models.gcr import ImageInfo
+from auspex_core.gcp.gcr import get_image_info
 
 from .config import AppConfig
-from .gcr import ImageInfo, get_image_info
 from .exceptions import APIError, UserAPIError
 from .scan import scan_container
 from .types import ScanResultsType
+from .models import CompletedScan, ScanIn
 
 if os.getenv("DEBUG") == "1":
     import debugpy
@@ -29,36 +31,13 @@ if os.getenv("DEBUG") == "1":
 app = FastAPI()
 
 
-class ScanIn(BaseModel):
-    image: str
-    backend: str = "snyk"
-
-
-class CompletedScan(BaseModel):
-    image: ImageInfo
-    backend: str
-    scan: str
-
-    def dict(self, *args, **kwargs) -> dict[str, Any]:
-        # Instead of defining a custom JSON encoder, we just convert the
-        # datetime objects to POSIX timestamps here.
-        img = self.image.dict()
-        img["uploaded"] = self.image.uploaded.timestamp()
-        img["created"] = self.image.created.timestamp()
-        return {
-            "image": img,
-            "backend": self.backend,
-            "scan": self.scan,
-        }
-
-
 # TODO: improve exception handlers
 
 
 @app.exception_handler(ValidationError)
 async def handle_validation_error(request: Request, exc: ValidationError):
     # TODO: improve message
-    logger.error("A pydantic validation error occured", exception=exc)
+    logger.error("A pydantic validation error occured", exc)
     return JSONResponse(
         status_code=400,
         content={"detail": exc.errors()},
@@ -68,7 +47,7 @@ async def handle_validation_error(request: Request, exc: ValidationError):
 @app.exception_handler(APIError)
 async def handle_api_error(request: Request, exc: APIError):
     # TODO: improve message
-    logger.error("An exception occured", exception=exc)
+    logger.error("An exception occured", exc)
     return JSONResponse(status_code=400, content=exc.args)
 
 
@@ -128,8 +107,9 @@ async def log_completed_scan(
 @app.post("/scan", response_model=ScanLog)
 async def scan_endpoint(scan_request: ScanIn) -> ScanLog:
     """Scans a single container image."""
-    image_info = await get_image_info(scan_request.image)
+    image_info = await get_image_info(scan_request.image, AppConfig().project)
 
+    # NOTE: use image name from image_info instead?
     scan = await scan_container(
         image=scan_request.image,
         backend=scan_request.backend,
