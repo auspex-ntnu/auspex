ed5d14b8f551413ff0b91f89c497a4d9eb799e15
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-04-10 21:38:14 +0200

    Core: add int support for timestamp_ms_to_datetime

--- core/auspex_core/utils/time.py
+++ core/auspex_core/utils/time.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+from typing import Union
 
 
-def timestamp_ms_to_datetime(timestamp_ms: str) -> datetime:
+def timestamp_ms_to_datetime(timestamp_ms: Union[str, int]) -> datetime:
     """Convert a timestamp in milliseconds to a datetime object"""
     ts = int(timestamp_ms) / 1000
     return datetime.fromtimestamp(ts)
