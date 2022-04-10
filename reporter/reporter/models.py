1ae5463ec2ea3c740118cfcaad5fcfba549c0d7b
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-04-10 21:38:58 +0200

    Reporter: remove collection field from ReportRequestIn

--- reporter/reporter/models.py
+++ reporter/reporter/models.py
@@ -1,13 +1,12 @@
 from typing import List
 from pydantic import BaseModel, Field, validator
 from .frontends import SUPPORTED_FRONTENDS
-from auspex_core.gcp.env import COLLECTION_LOGS
+from .config import AppConfig
 
 
 class ReportRequestIn(BaseModel):
     document_id: list[str] = Field(..., min_items=1)
     ignore_failed: bool = False
-    collection: str = COLLECTION_LOGS
     format: str = "latex"
     # rename to style?
 
