0f10d96a21c3255da866deb66f051f69b70452f6
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-04-11 12:41:49 +0200

    Move business logic from `firestore.py` to `db.py`

--- reporter/reporter/db.py
+++ reporter/reporter/db.py
@@ -1,26 +1,36 @@
-from datetime import datetime
-
-from typing import TYPE_CHECKING
+from datetime import datetime, timedelta
+from typing import TYPE_CHECKING, Any, Optional, Union
 
 from loguru import logger
-
 
 if TYPE_CHECKING:
     from google.cloud.firestore_v1.collection import CollectionReference
 
 from auspex_core.gcp.firestore import get_firestore_client
-from .config import AppConfig
-from auspex_core.models.scan import ReportData, ParsedVulnerabilities
-from google.cloud.firestore_v1.types.write import WriteResult
-from google.api_core.exceptions import InvalidArgument
-from google.cloud import firestore  # type: ignore # mypy doesn't understand this import
-
-
 from auspex_core.models.cve import SEVERITIES
-from .types import ScanTypeSingle
+from auspex_core.models.scan import ParsedVulnerabilities, ReportData
+from google.api_core.exceptions import InvalidArgument
+from google.cloud.firestore import SERVER_TIMESTAMP
+from google.cloud.firestore_v1.async_document import AsyncDocumentReference
+from google.cloud.firestore_v1.async_query import AsyncQuery
+from google.cloud.firestore_v1.types import WriteResult
+from google.cloud.firestore_v1.types.write import WriteResult
+from pydantic import ValidationError
 
+from .config import AppConfig
+from .types import ScanTypeSingle
+from .types.protocols import ScanTypeSingle
 
 # TODO: replace arg with protocol type to support multiple backends
+
+# async def log_scan(scan: ScanTypeSingle) -> WriteResult:
+#     client = get_firestore_client()
+#     transaction = client.transaction()
+# async def _log_scan(
+#     transaction: firestore.AsyncTransaction, scan: ScanTypeSingle
+# ) -> WriteResult:
+
+
 async def log_scan(scan: ScanTypeSingle) -> WriteResult:
     """Store results of parsed container scan in the database."""
     r = ReportData(
@@ -74,3 +84,87 @@ async def log_scan(scan: ScanTypeSingle) -> WriteResult:
                 raise
 
     return result
+
+
+async def get_prev_scans(
+    scan: ScanTypeSingle,
+    collection: str,
+    max_age: Union[timedelta, datetime],
+    ignore_self: bool = True,
+    by_image: bool = True,
+    skip_historical: bool = True,
+) -> list[ReportData]:
+    """Given a single image scan, find all previous scans going back
+    to a certain date.
+
+    Parameters
+    ----------
+    scan : `ScanType`
+        The scan whose image to find previous reports of.
+    collection : `str`
+        The firestore collection to search for reports in.
+    max_age : `Union[timedelta, datetime]`
+        Maximum age of report to retrieve.
+        Can be an absolute point in time (datetime) or a maximum age (timedelta).
+    ignore_self : `bool`, optional
+        If true, does not include the input scan in the returned list, by default True
+    by_image : `bool`, optional
+        If true, returns reports by image creation date instead of scan date, by default True
+    skip_historical : `bool`, optional
+        If true, skips historical reports, by default True
+
+    Returns
+    -------
+    `list[ReportData]`
+        List of previous scan reports.
+    """
+    if isinstance(max_age, timedelta):
+        cutoff = datetime.now() - max_age
+    else:
+        cutoff = max_age
+
+    client = get_firestore_client()
+    col = client.collection(collection)
+    query = col.where("image.image", "==", scan.image.image)  # type: AsyncQuery
+
+    # Perform filtering by date client-side instead of using composite query
+    # This will require more database reads and memory, but saves us from
+    # having to create a composite index
+    reports = []  # type: list[ReportData]
+    async for doc in query.stream():
+        d = doc.to_dict()
+        if not d:  # always check for falsey values
+            continue
+
+        # Ignore self when searching for previous scans
+        if ignore_self and d.get("id") == scan.id:
+            continue
+
+        # Ignore historical (older versions of) reports
+        if skip_historical and d.get("historical") == True:
+            continue
+
+        # Verify that doc has a timestamp and retrieve it
+        if by_image:
+            # XXX: use doc.get instead for nested fields? Is that an extra read?
+            img = d.get("image", {})  # type: dict[str, Any]
+            timestamp = img.get("created")  # type: Optional[datetime]
+        else:
+            timestamp = d.get("timestamp")
+        if not timestamp:
+            k = "image.created" if by_image else "timestamp"
+            logger.warning(f"Document '{doc.id}' has no key '{k}'.")
+            continue
+
+        # Use timezone from doc when comparing
+        if timestamp > cutoff.replace(tzinfo=timestamp.tzinfo):
+            try:
+                r = ReportData(**d)
+            except ValidationError:
+                logger.exception(f"Unable to parse document '{doc.id}'")
+                continue
+            reports.append(r)
+
+    # TODO: assert no duplicate ids?
+    return reports
+
--- reporter/reporter/utils/firestore.py
+++ reporter/reporter/utils/firestore.py
@@ -1,16 +1,12 @@
-from datetime import datetime, timedelta
-from typing import Any, Optional, Union
-
 from auspex_core.gcp.firestore import get_document, get_firestore_client
-from auspex_core.models.scan import ReportData
+
 from fastapi.exceptions import HTTPException
 from google.cloud.firestore_v1 import DocumentSnapshot
-from google.cloud.firestore_v1.async_query import AsyncQuery
+
 from loguru import logger
-from pydantic import ValidationError
+
 
 from ..config import AppConfig
-from ..types.protocols import ScanTypeSingle
 
 
 async def get_firestore_document(document_id: str, collection: str) -> DocumentSnapshot:
@@ -92,79 +88,3 @@ async def get_firestore_documents(
         raise HTTPException(500, msg)
 
     return docs
-
-
-async def get_prev_scans(
-    scan: ScanTypeSingle,
-    collection: str,
-    max_age: Union[timedelta, datetime],
-    ignore_self: bool = True,
-    by_image: bool = True,
-) -> list[ReportData]:
-    """Given a single image scan, find all previous scans going back
-    to a certain date.
-
-    Parameters
-    ----------
-    scan : `ScanType`
-        The scan whose image to find previous reports of.
-    collection : `str`
-        The firestore collection to search for reports in.
-    max_age : `Union[timedelta, datetime]`
-        Maximum age of report to retrieve.
-        Can be an absolute point in time (datetime) or a maximum age (timedelta).
-    ignore_self : `bool`, optional
-        If true, does not include the input scan in the returned list, by default True
-    by_image : `bool`, optional
-        If true, returns reports by image creation date instead of scan date, by default True
-
-    Returns
-    -------
-    `list[ReportData]`
-        List of previous scan reports.
-    """
-    if isinstance(max_age, timedelta):
-        cutoff = datetime.now() - max_age
-    else:
-        cutoff = max_age
-
-    client = get_firestore_client()
-    col = client.collection(collection)
-    query = col.where("image.image", "==", scan.image.image)  # type: AsyncQuery
-
-    # Perform filtering by date client-side instead of using composite query
-    # This will require more database reads and memory, but saves us from
-    # having to create a composite index
-    reports = []  # type: list[ReportData]
-    async for doc in query.stream():
-        d = doc.to_dict()
-        if not d:  # always check for falsey values
-            continue
-
-        # Ignore self when searching for previous scans
-        if ignore_self and d.get("id") == scan.id:
-            continue
-
-        # Verify that doc has a timestamp and retrieve it
-        if by_image:
-            # XXX: use doc.get instead for nested fields? Is that an extra read?
-            img = d.get("image", {})  # type: dict[str, Any]
-            timestamp = img.get("created")  # type: Optional[datetime]
-        else:
-            timestamp = d.get("timestamp")
-        if not timestamp:
-            k = "image.created" if by_image else "timestamp"
-            logger.warning(f"Document '{doc.id}' has no key '{k}'.")
-            continue
-
-        # Use timezone from doc when comparing
-        if timestamp > cutoff.replace(tzinfo=timestamp.tzinfo):
-            try:
-                r = ReportData(**d)
-            except ValidationError:
-                logger.exception(f"Unable to parse document '{doc.id}'")
-                continue
-            reports.append(r)
-
-    # TODO: assert no duplicate ids?
-    return reports
