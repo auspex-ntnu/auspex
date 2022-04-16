abde2e22720fd92f62cc943ab30b4d4827a09aab
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-04-16 20:01:33 +0200

    Add ScanRequest field descriptions

--- core/auspex_core/models/api/scan.py
+++ core/auspex_core/models/api/scan.py
@@ -6,10 +6,32 @@ from pydantic import BaseModel, Field
 
 DEFAULT_FORMAT = os.getenv("REPORTER_DEFAULT_FORMAT") or "latex"
 
-
+# FIXME: why is this shared
 class ScanRequest(BaseModel):
-    images: list[str] = Field(default_factory=list)
-    repository: Optional[str] = None
-    format: str = DEFAULT_FORMAT  # support multiple formats?
-    backend: str = "snyk"
-    ignore_failed: bool = False
+    images: list[str] = Field(
+        default_factory=list,
+        description="List of image names to scan.",
+    )
+
+    repository: Optional[str] = Field(
+        default=None,
+        description="Repository name to scan images of. Supercedes images.",
+    )
+
+    format: str = Field(
+        default=DEFAULT_FORMAT,
+        description="Format of report.",
+    )  # FIXME: This should be defined in reporter's model
+
+    backend: str = Field(
+        default="snyk",
+        description="Scanning backend to use.",
+    )  # FIXME: This should be defined in scanner's model
+
+    ignore_failed: bool = Field(
+        default=False,
+        description="Whether or not to ignore failed scans. Failed scans raise exception if False.",
+    )  # TODO: Get default value from config
+
+    class Config:
+        extra = "allow"  # allow extra fields in the request
