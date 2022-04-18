a6e34b5dd3b112aba9cee866610972189108510d
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-04-18 13:32:20 +0200

    API: Add timeout envvars for services

--- restapi/restapi/config.py
+++ restapi/restapi/config.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseSettings, Field
 
 
@@ -5,3 +6,5 @@ class AppConfig(BaseSettings):
     url_reporter: str = Field(..., env="URL_REPORTER")
     url_scanner: str = Field(..., env="URL_SCANNER")
     collection_reports: str = Field(..., env="COLLECTION_REPORTS")
+    timeout_reporter: Optional[float] = Field(None, env="TIMEOUT_REPORTER")
+    timeout_scanner: Optional[float] = Field(None, env="TIMEOUT_SCANNER")
