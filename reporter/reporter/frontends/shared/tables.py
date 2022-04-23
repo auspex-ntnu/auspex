993fa58087ce0245b69b201fa6e9ddb78b334676
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-04-23 12:56:49 +0200

    Reporter: Put all tables in `shared.tables.py`

--- reporter/reporter/frontends/shared/tables.py
+++ reporter/reporter/frontends/shared/tables.py
@@ -64,3 +64,63 @@ def top_vulns_table(
     else:
         title = f"Top {len(most_severe)} Most Critical Vulnerabilities"
     return TableData(title, header, rows)
+
+
+def statistics_table(report: Union[ScanTypeSingle, ScanTypeAggregate]) -> TableData:
+    columns = [
+        "Median CVSS",
+        "Mean CVSS",
+        "CVSS Stdev",
+        "Max CVSS",
+        "L",
+        "M",
+        "H",
+        "C",
+        "# Vulns",
+    ]
+    if isinstance(report, ScanTypeAggregate):
+        columns.insert(0, "Image")
+
+    dist = report.get_distribution_by_severity()
+    prio = ["critical", "high", "medium", "low"]
+    # This is flimsy and should be refactored and moved to
+    # a separate function.
+    # We rely on the order defined in the list `prio` above.
+    highest_severity = "low"  # default to low
+    for p in prio:
+        if dist.get(p):
+            highest_severity = p
+            break
+
+    highest_severity = highest_severity.title()
+    rows = []
+    if isinstance(report, ScanTypeAggregate):
+        for r in report.reports:
+            row = _get_singlereport_statistics_row(r)
+            row.insert(0, r.image.image)
+    else:
+        rows.append(_get_singlereport_statistics_row(report))
+
+    assert len(rows[0]) == len(columns)
+
+    return TableData(
+        title="Statistics",
+        header=columns,
+        rows=rows,
+    )
+
+
+def _get_singlereport_statistics_row(report: ScanTypeSingle) -> list[Any]:
+    dist = report.get_distribution_by_severity()
+    row = [
+        format_decimal(report.cvss.median),
+        format_decimal(report.cvss.mean),
+        format_decimal(report.cvss.stdev),
+        format_decimal(report.cvss.max),
+        dist["low"],
+        dist["medium"],
+        dist["high"],
+        dist["critical"],
+        dist["low"] + dist["medium"] + dist["high"] + dist["critical"],
+    ]
+    return row
