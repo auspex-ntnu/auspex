a2d3f6a84ae374cd953d3c7fbd43c6b06389d6f8
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-04-24 12:24:00 +0200

    Reporter: Add latex `add_row` function

--- reporter/reporter/frontends/latex/table.py
+++ reporter/reporter/frontends/latex/table.py
@@ -1,6 +1,6 @@
 from typing import Any, Literal, Optional, Sequence, Union
 
-from pylatex import LongTable, LongTabu, LongTabularx, MultiColumn
+from pylatex import LongTable, LongTabu, LongTabularx, MultiColumn, NoEscape
 
 LongTableType = Union[LongTable, LongTabu, LongTabularx]
 
@@ -28,4 +28,10 @@ def init_longtable(
         table.add_row((MultiColumn(len(columns), align=align, data=end_footer_text),))
     table.add_hline()
     table.end_table_last_footer()
+    return table
+
+
+def add_row(table: LongTableType, row: Sequence[Any]) -> LongTableType:
+    """Adds a row to a longtable and wraps each cell text in a `pylatex.NoEscape`."""
+    table.add_row([NoEscape(c) for c in row])
     return table
