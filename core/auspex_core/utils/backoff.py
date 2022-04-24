453439adc5e4ab0de69b6d4a4b17bf610b6a9379
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-04-24 12:30:28 +0200

    Core: Limit args and kwargs length on backoff logging

--- core/auspex_core/utils/backoff.py
+++ core/auspex_core/utils/backoff.py
@@ -2,11 +2,21 @@ from typing import Any
 
 from loguru import logger
 
+MAX_LEN_ARGS = 250
+MAX_LEN_KWARGS = 250
+
 
 def on_backoff(details: dict[str, Any]) -> None:
     """Generic callback function that can be fired whenever a backoff is triggered."""
-    # Directly ripped off from https://github.com/litl/backoff#event-handlers
+    args = str(details["args"])
+    if len(args) > MAX_LEN_ARGS:
+        args = args[:MAX_LEN_ARGS] + "..."
+    kwargs = str(details["args"])
+    if len(kwargs) > MAX_LEN_KWARGS:
+        kwargs = kwargs[:MAX_LEN_KWARGS] + "..."
     logger.debug(
-        "Backoff triggered after {tries} tries calling function {target} "
-        "with args {args} and kwargs {kwargs}".format(**details)
+        (
+            "Backoff triggered after {tries} tries calling function {target}"
+            "with args {args} and kwargs {kwargs}"
+        ).format(**details, args=args, kwargs=kwargs)
     )
