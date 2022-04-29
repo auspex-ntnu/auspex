ba643ba1029fd47b06e664d717f21e064324354e
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-04-29 16:38:51 +0200

    Reporter: TableData: add `caption` and `description`

--- reporter/reporter/frontends/shared/models.py
+++ reporter/reporter/frontends/shared/models.py
@@ -24,6 +24,8 @@ class TableData(NamedTuple):
     title: str
     header: list[str]  # column names
     rows: list[list[Any]]  # each row is a list of len(header)
+    caption: str = ""
+    description: str = ""
 
 
 class PlotData(NamedTuple):
