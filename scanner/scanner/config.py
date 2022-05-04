f215954779a3784260e120a544198416a1bd2eac
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-05-04 11:52:01 +0200

    Scanner: make GOOGLE_APPLICATION_CREDENTIALS mandatory

--- scanner/scanner/config.py
+++ scanner/scanner/config.py
@@ -6,6 +6,4 @@ class AppConfig(BaseSettings):
     collection_scans: str = Field(..., env="COLLECTION_SCANS")
     bucket_scans: str = Field(..., env="BUCKET_SCANS")
     project: str = Field(..., env="GOOGLE_CLOUD_PROJECT")
-    google_credentials: Optional[str] = Field(
-        None, env="GOOGLE_APPLICATION_CREDENTIALS"
-    )
+    google_credentials: str = Field(..., env="GOOGLE_APPLICATION_CREDENTIALS")
