bf0b029956b7c88982443884476c9467c9a3a043
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-05-04 11:49:15 +0200

    API: remove collection from config

--- restapi/restapi/config.py
+++ restapi/restapi/config.py
@@ -5,6 +5,5 @@ from pydantic import BaseSettings, Field
 class AppConfig(BaseSettings):
     url_reporter: str = Field(..., env="URL_REPORTER")
     url_scanner: str = Field(..., env="URL_SCANNER")
-    collection_reports: str = Field(..., env="COLLECTION_REPORTS")
     timeout_reporter: Optional[float] = Field(None, env="TIMEOUT_REPORTER")
     timeout_scanner: Optional[float] = Field(None, env="TIMEOUT_SCANNER")
