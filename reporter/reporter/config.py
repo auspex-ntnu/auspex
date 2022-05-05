7ccb8219169b81f125f4a7f68b7429c2da94b38a
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-05-05 13:35:17 +0200

    reporter: Add URL_SCANNER, remove bucket from config

--- reporter/reporter/config.py
+++ reporter/reporter/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings, Field
 class AppConfig(BaseSettings):
     bucket_scans: str = Field(..., env="BUCKET_SCANS")
     bucket_reports: str = Field(..., env="BUCKET_REPORTS")
-    collection_scans: str = Field(..., env="COLLECTION_SCANS")
     collection_reports: str = Field(..., env="COLLECTION_REPORTS")
+    url_scanner: str = Field(..., env="URL_SCANNER")
     trend_weeks: int = Field(26, env="REPORTER_TREND_WEEKS")
     debug: bool = Field(False, env="DEBUG")
