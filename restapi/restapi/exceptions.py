11c27dfd9381a19be0bea5b48652aa66bc893fd6
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-05-05 16:19:08 +0200

    API: add HTTPStatusError exc handler

--- restapi/restapi/exceptions.py
+++ restapi/restapi/exceptions.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from google.api_core.exceptions import FailedPrecondition
 from fastapi.responses import PlainTextResponse
 from loguru import logger
+from httpx import HTTPStatusError
 
 
 def handle_FailedPrecondition(
@@ -13,10 +14,19 @@ def handle_FailedPrecondition(
     return PlainTextResponse(str(exc.message), status_code=500)
 
 
+def handle_HTTPStatusError(req: Request, exc: HTTPStatusError) -> PlainTextResponse:
+    """Handles HTTPStatusError which stem from failed HTTP requests by the httpx module."""
+    logger.error(exc)
+    return PlainTextResponse(
+        content=exc.response.text, status_code=exc.response.status_code
+    )
+
+
 def install_handlers(app: FastAPI):
     """Installs custom exception handlers for the FastAPI app."""
     handlers = {
         FailedPrecondition: handle_FailedPrecondition,
+        HTTPStatusError: handle_HTTPStatusError,
     }
     for exc, handler in handlers.items():
         app.add_exception_handler(exc, handler)
