b0c71255c036e58a6b40e97e5a24bc3811ec7681
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-05-07 15:54:34 +0200

    Scanner: refactor and fix SnykScanResults tests

--- scanner/tests/backends/test_snyk.py
+++ scanner/tests/backends/test_snyk.py
@@ -1,20 +1,31 @@
+from subprocess import CompletedProcess
+from unittest.mock import Mock
 from scanner.backends.snyk import SnykScanResults
 
 
-def _get_scan() -> SnykScanResults:
-    return SnykScanResults(stderr="", stdout="Ok!", returncode=0)
+def test_snykscanresults_ok() -> None:
+    mockprocess = Mock(spec=CompletedProcess)
+    mockprocess.returncode = 0
+    scan = SnykScanResults(scan="{'status': 'ok'}", error="", process=mockprocess)
+    assert scan.process.returncode == 0
+    assert scan.ok
 
 
-def _get_scan_error() -> SnykScanResults:
-    return SnykScanResults(stderr="", stdout="Fail!", returncode=1)
+def test_snykscanresults_error() -> None:
+    mockprocess = Mock(spec=CompletedProcess)
+    mockprocess.returncode = 2
+    scan = SnykScanResults(scan="", error="Fail!", process=mockprocess)
+    assert scan.process.returncode != 0
+    assert not scan.ok
 
 
-def test_model_snykscanresults() -> None:
-    # verify that we can instantiate the model
-    assert _get_scan()
+def test_snykscanresults_1() -> None:
+    """`Snyk container test` can have an exit code of 1, which is not an error.
+    This test verifies that 1 is not treated as an error.
 
-
-def test_snyk_model_error() -> None:
-    s = _get_scan_error()
-    assert not s.ok()
-    assert s.returncode != 0
+    See: SnykScanResults.ok
+    """
+    mockprocess = Mock(spec=CompletedProcess)
+    mockprocess.returncode = 1
+    scan = SnykScanResults(scan="", error="Fail!", process=mockprocess)
+    assert scan.ok
