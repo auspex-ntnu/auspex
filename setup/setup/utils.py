078e8d96c950b737a9e98f69d30c096c1651ccf2
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-05-07 13:33:48 +0200

    Setup: Fix missing headers for container metadata fetching

--- setup/setup/utils.py
+++ setup/setup/utils.py
@@ -4,13 +4,15 @@ import httpx
 
 def get_metadata() -> dict[str, Any]:
     r = httpx.get(
-        "http://metadata.google.internal/computeMetadata/v1/instance/attributes/metadata"
+        "http://metadata.google.internal/computeMetadata/v1/instance/attributes/metadata",
+        headers={"Metadata-Flavor": "Google"},
     )
     return r.json()
 
 
 def get_project_id() -> str:
     r = httpx.get(
-        "http://metadata.google.internal/computeMetadata/v1/project/project-id"
+        "http://metadata.google.internal/computeMetadata/v1/project/project-id",
+        headers={"Metadata-Flavor": "Google"},
     )
     return str(r.text)  # don't handle errors
