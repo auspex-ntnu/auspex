b3590c5128c04ac42a815e22305048f545e930a3
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-05-18 17:31:34 +0200

    Scanner: remove Protocol ellipses, add docstrings

--- scanner/scanner/types.py
+++ scanner/scanner/types.py
@@ -5,26 +5,23 @@ from pydantic import BaseModel
 
 
 class ScanResultsType(Protocol):
-    """Abstract base class for scan results from any backend."""
+    """Interface for scan results from any backend."""
 
     @property
     def ok(self) -> bool:
         """Returns success status of scan."""
-        ...
 
     @property
     def scan(self) -> str:
         """Returns scan results as a JSON-encoded string."""
-        ...
 
     @property
     def error(self) -> dict[str, Any]:
         """Returns error message for scan (if any)."""
-        ...
 
     @property
     def backend(self) -> str:
-        ...
+        """Name of the backend used to perform the scan."""
 
     def dict(self, *args, **kwargs) -> dict[str, Any]:
-        ...
+        """Serializes the scan results to a dict."""
