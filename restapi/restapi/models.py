3c86ba3e37ca6ebd5748d5c1e1de9d37b788460d
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-05-18 17:30:20 +0200

    API: don't use conlist in ScanReportRequest

--- restapi/restapi/models.py
+++ restapi/restapi/models.py
@@ -6,7 +6,7 @@ from pydantic import Field, conlist, validator, root_validator
 class ScanReportRequest(ReportRequestBase):
     """Model used for requesting both a scan and report for one or more images."""
 
-    # This clas facilitates a more natural user interface for the /reports endpoint,
+    # This class facilitates a more natural user interface for the /reports endpoint,
     # due to the fact that a scan and report are often requested together.
     #
     # Users are not expected to know scan IDs, but are expected to know image names.
@@ -15,16 +15,11 @@ class ScanReportRequest(ReportRequestBase):
     # The model is based on the ReportRequestBase model, which is a base model for
     # all report requests.
 
-    images: conlist(str, min_items=1, unique_items=True,) = Field(
+    images: list[str] = Field(
         default_factory=list,
-        description="List of images to scan and report.",
+        description="List of image names to scan and report.",
+        min_items=1,
     )
-
-    # images: list[str] = Field(
-    #     default_factory=list,
-    #     description="List of image names to scan and report.",
-    #     min_items=1,
-    # )
 
     # NYI: repository
 
@@ -36,7 +31,7 @@ class ScanReportRequest(ReportRequestBase):
     @root_validator()
     def root_validator(cls, values: dict[str, Any]) -> dict[str, Any]:
         # Ensure that at at least two images are provided if aggregate is True
-        if values["aggregate"] and len(values["images"]) < 2:
+        if values.get("aggregate") and len(values.get("images", [])) < 2:
             raise ValueError(
                 "At least two images must be provided if aggregate is True."
             )
