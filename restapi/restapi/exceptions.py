cf43d2b8bc234497e94d2e5f5f29ba075ba6ca92
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-05-18 17:30:41 +0200

    API: improve FastAPI exception handling

--- restapi/restapi/exceptions.py
+++ restapi/restapi/exceptions.py
@@ -1,26 +1,60 @@
-from typing import TYPE_CHECKING, Any, Callable, Type, TypeVar, Union
 from fastapi import FastAPI, Request
-from google.api_core.exceptions import FailedPrecondition
-from fastapi.responses import PlainTextResponse
+from google.api_core.exceptions import FailedPrecondition, BadRequest, GoogleAPIError
+from fastapi.responses import JSONResponse
 from loguru import logger
 from httpx import HTTPStatusError
 
 
-def handle_FailedPrecondition(
+async def _handle_exception(
+    request: Request, exc: Exception, code: int = 500, prefix: str = ""
+) -> JSONResponse:
+    args = getattr(exc, "args")
+    a = args[0] if args else str(exc)
+    p = f"{prefix}: " if prefix else ""
+
+    # NOTE: we can't get the request body here
+    #
+    # See: https://github.com/tiangolo/fastapi/issues/394
+    #      https://stackoverflow.com/a/71811390
+    #
+    # However, we can add a middleware that stores the request body
+    # if really want it: https://github.com/tiangolo/fastapi/issues/394#issuecomment-994665859
+
+    content = {
+        "detail": f"{p}{a}",
+    }
+    return JSONResponse(status_code=code, content=content)
+
+
+async def handle_FailedPrecondition(
     request: Request, exc: FailedPrecondition
-) -> PlainTextResponse:
+) -> JSONResponse:
     """Handles FailedPrecondition exceptions which usually stem from
     failed or malformed Firestore queries."""
-    logger.error(exc)  # NOTE: can we just log the exception like this?
-    return PlainTextResponse(str(exc.message), status_code=500)
+    logger.error(f"Firestore error: {exc}")
+    return await _handle_exception(request, exc, code=500, prefix="Firestore error")
 
 
-def handle_HTTPStatusError(req: Request, exc: HTTPStatusError) -> PlainTextResponse:
+async def handle_google_BadRequest(request: Request, exc: BadRequest) -> JSONResponse:
+    """Handles GoogleAPIError which stem from failed Google API requests."""
+    logger.error(exc)
+    return await _handle_exception(
+        request, exc, code=400, prefix="Bad request to GCP service"
+    )
+
+
+async def handle_GoogleAPIError(request: Request, exc: GoogleAPIError) -> JSONResponse:
+    """Handles GoogleAPIError which stem from failed Google API requests."""
+    logger.error(exc)
+    return await _handle_exception(request, exc, code=400, prefix="Google API error")
+
+
+async def handle_HTTPStatusError(
+    request: Request, exc: HTTPStatusError
+) -> JSONResponse:
     """Handles HTTPStatusError which stem from failed HTTP requests by the httpx module."""
     logger.error(exc)
-    return PlainTextResponse(
-        content=exc.response.text, status_code=exc.response.status_code
-    )
+    return await _handle_exception(request, exc, code=400, prefix="HTTP error")
 
 
 def install_handlers(app: FastAPI):
@@ -29,6 +63,8 @@ def install_handlers(app: FastAPI):
     handlers = {
         FailedPrecondition: handle_FailedPrecondition,
         HTTPStatusError: handle_HTTPStatusError,
+        BadRequest: handle_google_BadRequest,
+        GoogleAPIError: handle_GoogleAPIError,
     }
     for exc, handler in handlers.items():
         app.add_exception_handler(exc, handler)  # type: ignore
