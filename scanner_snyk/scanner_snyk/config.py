75bd50a77de9eef044ef3a6b0230cfcafb2ff164
Peder Hovdan Andresen <pedeha@stud.ntnu.no>
2022-06-12 12:51:55 +0200

    ScannerSnyk: Remove unused config values

--- scanner_snyk/scanner_snyk/config.py
+++ scanner_snyk/scanner_snyk/config.py
@@ -4,7 +4,5 @@ from pydantic import BaseSettings, Field
 
 
 class AppConfig(BaseSettings):
-    collection_scans: str = Field(..., env="COLLECTION_SCANS")
-    bucket_scans: str = Field(..., env="BUCKET_SCANS")
     project: str = Field(..., env="GOOGLE_CLOUD_PROJECT")
     google_credentials: str = Field(..., env="GOOGLE_APPLICATION_CREDENTIALS")
